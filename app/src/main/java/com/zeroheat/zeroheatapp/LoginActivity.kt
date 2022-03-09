fcac5c7cc135668d7d90abd7b8679dddbb375b81
jeongdongjun <dongjunkorea@naver.com>
2022-03-09 20:16:48 +0900

    기본세팅5 항상 빌드 그래들 할때 buildFeqtures{dataBinding = true}해줘야됨

--- app/src/main/java/com/zeroheat/zeroheatapp/LoginActivity.kt
+++ app/src/main/java/com/zeroheat/zeroheatapp/LoginActivity.kt
@@ -1,14 +1,16 @@
 package com.zeroheat.zeroheatapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.zeroheat.zeroheatapp.databinding.ActivityLoginBinding
 
 class LoginActivity : BaseBootCamp() {
 
+    lateinit var binding : ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         setupEvents()
         setValues()
     }
